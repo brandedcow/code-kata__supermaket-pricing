acde1c03cbc0b32b47d597292fb7cb8500eb751c
Chris Kang <chris.v.kang@gmail.com>
2021-09-13 12:55:46 -0700

    Added promo code start and end dates

--- PromoCode.js
+++ PromoCode.js
@@ -1,10 +1,20 @@
 const { v4: uuidv4 } = require('uuid')
+const isWithinInterval = require('date-fns/isWithinInterval')
 
 class PromoCode {
-  constructor() {
+  constructor(start = new Date(), end = new Date()) {
     this.id = uuidv4()
+    this.startDate = start
+    this.endDate = end
     this.targets = []
     this.modifier = price => price
+  }
+  
+  isValid() {
+    return isWithinInterval(
+      new Date(),
+      { start: this.startDate, end: this.endDate }
+    )
   }
 
   affects(itemId) {
